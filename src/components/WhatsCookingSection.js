d7e6bb81a43fe3d43945755ca41802fe17877d2b
Lehanrathnayaka <lehanrathnayakeexam@gmail.com>
2025-02-06 22:35:53 +0530

    make mobile responsive

--- src/components/WhatsCookingSection.js
+++ src/components/WhatsCookingSection.js
@@ -6,24 +6,18 @@ function WhatsCookingSection() {
     <section className="whats-cooking">
       <div className="container">
         <h2>What's Cooking?</h2>
-        <div className="p">
-          <div>
-            {" "}
-            <p>
-              With over [X] years of experience, I've been sharing the authentic
-              tastes of Sri Lankan cuisine with food lovers from around the
-              world. Join me in my kitchen to cook, laugh, and explore the rich
-              heritage of Sri Lanka's food culture.
-            </p>
-          </div>
-          <div>
-            {" "}
-            <p>
-              Whether you're craving spicy curries, sweet desserts, or savory
-              snacks, I offer a variety of hands-on cooking classes tailored to
-              your taste.
-            </p>
-          </div>
+        <div className="content">
+          <p>
+            With over [X] years of experience, I've been sharing the authentic
+            tastes of Sri Lankan cuisine with food lovers from around the
+            world. Join me in my kitchen to cook, laugh, and explore the rich
+            heritage of Sri Lanka's food culture.
+          </p>
+          <p>
+            Whether you're craving spicy curries, sweet desserts, or savory
+            snacks, I offer a variety of hands-on cooking classes tailored to
+            your taste.
+          </p>
         </div>
       </div>
     </section>
